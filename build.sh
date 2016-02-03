8289d26d16cd83595095fc6c2edd24fc17bee9dc
John Jacquay <jacquayj@ufl.edu>
2016-02-03 14:56:18 -0500

    Making repository go get-able

--- build.sh
+++ build.sh
@@ -5,7 +5,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Compile gorods.o, build libgorods.a with iRods C API
-(cd $DIR/src/gorods/lib; gcc -c -fPIC wrapper.c -o build/gorods.o -I./include -I./irods/lib/core/include -I./irods/lib/api/include -I./irods/lib/md5/include -I./irods/lib/sha1/include -I./irods/server/core/include -I./irods/server/icat/include -I./irods/server/drivers/include -I./irods/server/re/include; cd build; ar rcs libgorods.a *.o)
-
-# Compile & run Go code
-(cd $DIR/src/gorods; go install tester && ../../bin/tester)
+(cd $DIR/lib; gcc -c -fPIC wrapper.c -o build/gorods.o -I./include -I./irods/lib/core/include -I./irods/lib/api/include -I./irods/lib/md5/include -I./irods/lib/sha1/include -I./irods/server/core/include -I./irods/server/icat/include -I./irods/server/drivers/include -I./irods/server/re/include; cd build; ar rcs libgorods.a *.o)
