840c92196fa1245e14d2d6b32bbdbf8a422ef723
John Jacquay <jacquayj@ufl.edu>
2016-02-22 15:36:20 -0500

    Updated internal build script for go get functionaility

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Compile gorods.o, build libgorods.a with iRods C API
-(cd $DIR/lib; gcc -c -fPIC wrapper.c -o build/gorods.o -I./include -I./irods/lib/core/include -I./irods/lib/api/include -I./irods/lib/md5/include -I./irods/lib/sha1/include -I./irods/server/core/include -I./irods/server/icat/include -I./irods/server/drivers/include -I./irods/server/re/include; cd build; ar rcs libgorods.a *.o)
+(cd $DIR/src/github.com/jjacquay712/GoRods/lib; gcc -c -fPIC wrapper.c -o build/gorods.o -I./include -I./irods/lib/core/include -I./irods/lib/api/include -I./irods/lib/md5/include -I./irods/lib/sha1/include -I./irods/server/core/include -I./irods/server/icat/include -I./irods/server/drivers/include -I./irods/server/re/include; cd build; ar rcs libgorods.a *.o)
+
+go install github.com/jjacquay712/GoRods && go install tester && bin/tester
+
