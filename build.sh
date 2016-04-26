d91d584af49eaf9dd2b5b5ef4803729aef7cbbbd
John Jacquay <john@johnjacquay.com>
2016-04-26 15:15:25 -0400

    Silenced output on build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Compile gorods.o, build libgorods.a with iRods C API
-(cd $DIR/src/github.com/jjacquay712/GoRods/lib; rm build/libgorods.a; rm build/gorods.o; gcc -ggdb -o build/gorods.o -c wrapper.c -I/usr/include/irods -Iinclude; ar rcs build/libgorods.a build/gorods.o)
+(cd $DIR/src/github.com/jjacquay712/GoRods/lib; rm -f build/libgorods.a; rm -f build/gorods.o; gcc -ggdb -o build/gorods.o -c wrapper.c -I/usr/include/irods -Iinclude; ar rcs build/libgorods.a build/gorods.o)
 
 # Replace 'tester' with your application's own package name
 go install github.com/jjacquay712/GoRods && go install tester && bin/tester
