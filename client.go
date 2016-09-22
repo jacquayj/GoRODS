715ea3c5edcd06f6c4bc2d8b1ab5aba8d8e48554
John Jacquay <john@bioteam.net>
2016-09-22 10:28:36 -0400

    Added DataObject and Connection entry points to client struct

--- client.go
+++ client.go
@@ -22,12 +22,12 @@ type Client struct {
 	ConnectErr error
 }
 
-// OpenConnection will create a new connection using the previously configured iRODS client. It will execute the handler,
+// OpenCollection will create a new connection using the previously configured iRODS client. It will execute the handler,
 // and close *Collection and *Collection automatically when your handler finishes execution.
 // Operations on a single connection are queued when shared between goroutines (iRODS C API
 // doesn't support concurrent operations on a single connection), so be sure to open up new connections
 // for long-running operations to prevent blocking between goroutines.
-func (cli *Client) OpenConnection(opts CollectionOptions, handler func(*Collection, *Connection)) error {
+func (cli *Client) OpenCollection(opts CollectionOptions, handler func(*Collection, *Connection)) error {
 	if cli.ConnectErr == nil {
 		if con, err := NewConnection(cli.Options); err == nil {
 			col, colEr := con.Collection(opts)
@@ -54,6 +54,63 @@ func (cli *Client) OpenConnection(opts CollectionOptions, handler func(*Collecti
 	return newError(Fatal, fmt.Sprintf("Can't open new connection: %v", cli.ConnectErr))
 }
 
+// OpenDataObject will create a new connection using the previously configured iRODS client. It will execute the handler,
+// and close *DataObj and *Collection automatically when your handler finishes execution.
+// Operations on a single connection are queued when shared between goroutines (iRODS C API
+// doesn't support concurrent operations on a single connection), so be sure to open up new connections
+// for long-running operations to prevent blocking between goroutines.
+func (cli *Client) OpenDataObject(path string, handler func(*DataObj, *Connection)) error {
+	if cli.ConnectErr == nil {
+		if con, err := NewConnection(cli.Options); err == nil {
+
+			obj, objEr := con.DataObject(path)
+
+			if objEr != nil {
+				return newError(Fatal, fmt.Sprintf("Can't open new connection: %v", objEr))
+			}
+
+			handler(obj, con)
+
+			if er := obj.col.Close(); er != nil {
+				return er
+			}
+			if er := con.Disconnect(); er != nil {
+				return er
+			}
+
+			return nil
+		} else {
+			return newError(Fatal, fmt.Sprintf("Can't open new connection: %v", err))
+		}
+	}
+
+	return newError(Fatal, fmt.Sprintf("Can't open new connection: %v", cli.ConnectErr))
+}
+
+// OpenConnection will create a new connection using the previously configured iRODS client. It will execute the handler,
+// and close *Collection automatically when your handler finishes execution.
+// Operations on a single connection are queued when shared between goroutines (iRODS C API
+// doesn't support concurrent operations on a single connection), so be sure to open up new connections
+// for long-running operations to prevent blocking between goroutines.
+func (cli *Client) OpenConnection(handler func(*Connection)) error {
+	if cli.ConnectErr == nil {
+		if con, err := NewConnection(cli.Options); err == nil {
+
+			handler(con)
+
+			if er := con.Disconnect(); er != nil {
+				return er
+			}
+
+			return nil
+		} else {
+			return newError(Fatal, fmt.Sprintf("Can't open new connection: %v", err))
+		}
+	}
+
+	return newError(Fatal, fmt.Sprintf("Can't open new connection: %v", cli.ConnectErr))
+}
+
 // New creates a test connection to an iRods iCAT server, and returns a *Client struct if successful.
 // EnvironmentDefined and UserDefined constants are used in ConnectionOptions{ Type: ... }).
 // When EnvironmentDefined is specified, the options stored in ~/.irods/irods_environment.json will be used.
@@ -64,9 +121,13 @@ func New(opts ConnectionOptions) (*Client, error) {
 
 	cli.Options = &opts
 
-	if _, err := NewConnection(cli.Options); err != nil {
+	if con, err := NewConnection(cli.Options); err != nil {
 		cli.ConnectErr = err
 		return nil, err
+	} else {
+		if er := con.Disconnect(); er != nil {
+			return nil, er
+		}
 	}
 
 	return cli, nil
